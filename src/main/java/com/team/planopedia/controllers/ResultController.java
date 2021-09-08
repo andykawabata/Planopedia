214d0eb47913281cfac3dee5263e2c90f711db36
Michael <mcleonar@uncg.edu>
2021-09-08 15:11:40 -0400

    Added support for input from the user and sending output to the HTML pages.

--- src/main/java/com/team/planopedia/controllers/ResultController.java
+++ src/main/java/com/team/planopedia/controllers/ResultController.java
@@ -4,7 +4,9 @@ import com.team.planopedia.modelsAndServices.restaurant.Restaurant;
 import com.team.planopedia.modelsAndServices.restaurant.RestaurantService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -14,11 +16,23 @@ public class ResultController {
     RestaurantService restaurantService;
     
     @GetMapping("/result")
-    public String result(){
-        
-        Restaurant restaurant = restaurantService.generateRestaurant("mycity", "12345", "Italian", 1);
-        
-        System.out.println(restaurant.getBasicInfo().toString());
+    public String result(Model model,
+                         @RequestParam("city") String city,                 //This variables are obtained via GET request from form page.
+                         @RequestParam("zipcode") String zipcode,
+                         @RequestParam("cuisine") String cuisine,
+                         @RequestParam("numpeople") String numPeople)
+    {
+
+
+        Restaurant restaurant = restaurantService.generateRestaurant(city, zipcode, cuisine, Integer.parseInt(numPeople));
+
+        System.out.println(city);
+        System.out.println(zipcode);
+        System.out.println(cuisine);
+        System.out.println(numPeople);
+
+
+        model.addAttribute("restaurant",restaurant);   //adding the Restaurant object to a model which is accessible in the HTML pages.
         
         return "result";
     }
