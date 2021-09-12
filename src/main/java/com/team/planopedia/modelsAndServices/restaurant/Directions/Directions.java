7b5dc158169a91e32c40ccb67bda89e156d15171
andykawabata <andykawabata@gmail.com>
2021-09-12 16:46:52 -0400

    revert

--- src/main/java/com/team/planopedia/modelsAndServices/restaurant/Directions/Directions.java
+++ src/main/java/com/team/planopedia/modelsAndServices/restaurant/Directions/Directions.java
@@ -1,15 +1,7 @@
 package com.team.planopedia.modelsAndServices.restaurant.Directions;
 
 
-public class Directions { //Absorb this into basic info or will it become for complex?
-
-    private String directionsURL;
-
-    public String getDirectionsURL() {
-        return directionsURL;
-    }
-
-    public void setDirectionsURL(String directionsURL) {
-        this.directionsURL = directionsURL;
-    }
+public class Directions {
+    
 }
+
--- src/main/java/com/team/planopedia/modelsAndServices/restaurant/Reviews/SingleReview.java
+++ src/main/java/com/team/planopedia/modelsAndServices/restaurant/Reviews/SingleReview.java
@@ -2,15 +2,4 @@ package com.team.planopedia.modelsAndServices.restaurant.Reviews;
 
 public class SingleReview {
     
-    private String rating;
-    private String text;
-    private String date;
-
-    public SingleReview(String rating, String text, String date) {
-        this.rating = rating;
-        this.text = text;
-        this.date = date;
-    }
-    
-    
 }
