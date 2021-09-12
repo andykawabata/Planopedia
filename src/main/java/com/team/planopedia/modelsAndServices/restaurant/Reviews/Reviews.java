34bbc837c243e19a1012640b53fea463427d33ef
andykawabata <andykawabata@gmail.com>
2021-09-12 16:43:01 -0400

    revert

--- src/main/java/com/team/planopedia/modelsAndServices/restaurant/Reviews/Reviews.java
+++ src/main/java/com/team/planopedia/modelsAndServices/restaurant/Reviews/Reviews.java
@@ -3,12 +3,4 @@ package com.team.planopedia.modelsAndServices.restaurant.Reviews;
 
 public class Reviews {
     
-    private SingleReview goodReview;
-    private SingleReview badReview;
-
-    public Reviews(SingleReview goodReview, SingleReview badReview) {
-        this.goodReview = goodReview;
-        this.badReview = badReview;
-    }
-    
 }
