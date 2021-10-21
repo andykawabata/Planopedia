e4620ee2d4a525ffe99c5e420d6ee4b8ce5cd69d
andykawabata <andykawabata@gmail.com>
2021-10-21 10:07:22 -0400

    login/register flow working

--- src/main/java/com/team/planopedia/repository/UserRepository.java
+++ src/main/java/com/team/planopedia/repository/UserRepository.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,String> {
-    public User findByGoogleEmail(String email);
+
 
 
 
