7b02cef33ce690fe5490f5ba4ad23103bd792697
andykawabata <andykawabata@gmail.com>
2021-10-28 16:29:53 -0400

    added logout functionality

--- src/main/java/com/team/planopedia/SecurityConfig.java
+++ src/main/java/com/team/planopedia/SecurityConfig.java
@@ -1,13 +1,21 @@
 package com.team.planopedia;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+
+
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
+    
+    @Autowired
+    private ClientRegistrationRepository clientRegistrationRepository;
     
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -19,8 +27,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
                 .loginPage("/auth/login-page")
                 .defaultSuccessUrl("/auth/success", true)
                 .and()
+            .logout()
+                .logoutSuccessHandler(oidcLogoutSuccessHandler())
+                .invalidateHttpSession(true)
+                .clearAuthentication(true)
+                .deleteCookies("JSESSIONID")
+                .and()
             .csrf().disable();
         
 
     }
+    
+    private OidcClientInitiatedLogoutSuccessHandler oidcLogoutSuccessHandler() { 
+        OidcClientInitiatedLogoutSuccessHandler successHandler = new OidcClientInitiatedLogoutSuccessHandler(clientRegistrationRepository);
+        successHandler.setPostLogoutRedirectUri("/");
+    return successHandler;
+}
 }
