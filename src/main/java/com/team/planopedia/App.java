70d3f16eba76331be96f68bc24be2225d22a647f
Michael <mcleonar@uncg.edu>
2021-11-03 16:00:01 -0400

    Framework for the algorithm to calculate scores and return the best restaurant is implemented as well as a testing method that uses the User DummyData method to populate the user tables. Needs comments but it is all working.

--- src/main/java/com/team/planopedia/App.java
+++ src/main/java/com/team/planopedia/App.java
@@ -1,11 +1,15 @@
 package com.team.planopedia;
 
+import com.team.planopedia.ChoiceAlgorithm.ChoiceMaker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
-		SpringApplication.run(App.class, args);
+		//SpringApplication.run(App.class, args);
+
+		ChoiceMaker test = new ChoiceMaker();
+		test.makeDecisionTester();
 	}
 }
